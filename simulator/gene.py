0db025d8c45284eb1ccaef070144579af53354c7
Tamar Av-Shalom <avshalom.tamar0@gmail.com>
2018-10-25 11:02:33 -0700

    stubbed out gene and variant classes

---
+++ simulator/gene.py
@@ -0,0 +1,42 @@
+from Bio.Seq import Seq
+
+
+class Gene:
+
+    def __init__(self, seq, chr, start_pos, stop_pos, elements):
+        """ create gene object with sequence, chromosome, start position, 
+        stop position and elements. 
+        elements tuples in this form (type, start, stop) where type can be one 
+        of the following: exon, intron, utr, promoter, enhancer
+        """
+        self.seq = seq
+        self.chr = chr
+        self.start_reg = start_pos
+        self.stop_reg = stop_pos
+        self.elements = []
+        for el in elements:
+            self.elements.append(el)
+
+    def get_exons():
+        """ returns all exons from gene """
+        return False
+
+    def get_introns():
+        """ returns all introns from gene """
+        return False
+
+    def get_upstream_utr():
+        """ returns the 5' utr """
+        return False
+
+    def get_downstream_utr():
+        """ returns the 3' utr """
+        return False
+
+    def get_promoter():
+        """ returns promoter """
+        return False
+
+    def get_enhancer():
+        """ returns all enhancer/silencers """
+        return False
---
+++ simulator/variants.py
@@ -0,0 +1,29 @@
+class Variants: 
+
+    def __init__(self, gene, inheretance, var1_template, var2_template):
+        """ 
+        creates variants object which has gene, inheretance, var1, var2 
+        """
+        self.gene = gene # may have to expand to phenotype 
+        self.inheretance = inheretance
+        self.var1_template = var1_template
+        self.var2_template = var2_template
+
+
+
+
+    def template_2_variant(): 
+        """ turns variant template into variant, returning a dict with the 
+        following keys: chrom, pos, id, ref, alt """
+        return {"chrom": 1, 
+                "pos": 1, 
+                "ID": "ABC",
+                "REF": "A"
+                "ALT": "C"}
+
+
+    def save_vcf_output(file_name, trio)
+        """ saves a vcf output of the variants
+        if  trio == false then it just saves the child
+        if trio == true it saves a child and 2 parents"""
+        return False
