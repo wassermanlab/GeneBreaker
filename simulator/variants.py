914f018a58433084a9a34282e10a53e9e1a4c2d5
Tamar Av-Shalom <avshalom.tamar0@gmail.com>
2018-10-26 17:09:50 -0700

    some more planning and comments on how to code this class properly

--- simulator/variants.py
+++ simulator/variants.py
@@ -7,15 +7,18 @@ class Variants:
         """ 
         creates variants object which has gene, inheritance, var1, var2 
         """
-        self.gene = Gene(gene_json) # may have to expand to phenotype 
-        self.inheritance = variants_json["INHERITANCE"]
-        self.var1_template = Variant(variants_json["VAR1"])
-        self.var2_template = Variant(variants_json["VAR2"])
-
+        try: 
+            self.gene = Gene(gene_json) # may have to expand to phenotype 
+            self.inheritance = variants_json["INHERITANCE"]
+            self.var1_template = Variant(variants_json["VAR1"]) ## here check that it follows the general variant rules
+            self.var2_template = Variant(variants_json["VAR2"])
+        except:
+            print("Check that the variants input is correct and follows the schema")
 
     def template_2_variant(): 
         """ turns variant template into variant, returning a dict with the 
         following keys: chrom, pos, id, ref, alt """
+        ## here turn each superclass variant into its respective child class
         return {"chrom": 1, 
                 "pos": 1, 
                 "ID": "ABC",
