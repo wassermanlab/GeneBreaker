cf01a8367c27a44994072b27449482e88b32f9c9
Tamar Av-Shalom <avshalom.tamar0@gmail.com>
2018-11-09 17:01:34 -0800

    finished refractor, everything is working !

--- simulator/tests/test_variants.py
+++ simulator/tests/test_variants.py
@@ -12,5 +12,25 @@ class VariantCreationTests(unittest.TestCase):
         config = "simulator/tests/testing_data/test.json"
         Variants(config).save_vcf_output("simulator/tests/testing_data/output.vcf")
 
+    def test_writing_file_paternal(self):
+        config = "simulator/tests/testing_data/paternal.json"
+        Variants(config).save_vcf_output("simulator/tests/testing_data/paternal.output.vcf")
+
+    def test_writing_file_maternal(self):
+        config = "simulator/tests/testing_data/maternal.json"
+        Variants(config).save_vcf_output("simulator/tests/testing_data/maternal.output.vcf")
+
+    def test_writing_file_denovo(self):
+        config = "simulator/tests/testing_data/denovo.json"
+        Variants(config).save_vcf_output("simulator/tests/testing_data/denovo.output.vcf")
+
+    def test_writing_file_biparental1(self):
+        config = "simulator/tests/testing_data/biparental_1.json"
+        Variants(config).save_vcf_output("simulator/tests/testing_data/biparental1.output.vcf")
+    
+    def test_writing_file_biparental2(self):
+        config = "simulator/tests/testing_data/biparental_2.json"
+        Variants(config).save_vcf_output("simulator/tests/testing_data/biparental2.output.vcf")
+
 if __name__ == '__main__':
     unittest.main()
--- simulator/variants.py
+++ simulator/variants.py
@@ -13,6 +13,7 @@ class Variants:
         try: 
             f = open(variants_file)
             variants_json = json.load(f)
+            variants_json
             self.transcript = Transcript(variants_json["GENE_NAME"], variants_json["GENE_INDEX"])
             self.inheritance = variants_json["INHERITANCE"]
             self.var1 = variants_json["VAR1"]
@@ -24,11 +25,13 @@ class Variants:
 
     def variants_2_VCF(self): 
         """ turns variant template into variant, string representing vcf """
-        vcf = "#CHROM\tPOS\tID\tREF\tALT\n"
+        h = "#CHROM\tPOS\tID\tREF\tALT\n"
+        r1 = ""
+        r2 = ""
         variants = [self.var1]
         if self.var2 != "NONE":
             variants.append(self.var2)
-        for var in variants:
+        for index, var in enumerate(variants):
             var_type = var["TYPE"]
             row = ""
             if var_type == "SNV":
@@ -37,8 +40,11 @@ class Variants:
             if var_type == "INDEL":
                 indel = Indel(var)
                 row = indel.get_vcf_row(self.transcript)
-            vcf = vcf + row + "\n"
-        return vcf
+            if index == 0:
+                r1 = row + "\n"
+            if index == 1:
+                r2 = row + "\n"
+        return (h, r1, r2)
 
 
     def save_vcf_output(self, file_name):
@@ -47,6 +53,59 @@ class Variants:
         if  trio == false then it just saves the child
         if trio == true it saves a child and 2 parents"""
         vcf = self.variants_2_VCF()
-        f = open(file_name,"w+")
-        f.write(vcf)
-        f.close()
+        if self.trio == 'SINGLE':
+            f = open(file_name,"w+")
+            f.write(vcf[0]+vcf[1]+vcf[2])
+            f.close()
+        elif self.trio == 'TRIO':
+            if self.inheritance == "DE-NOVO":
+                f = open(file_name+".child","w+")
+                f.write(vcf[0]+vcf[1]+vcf[2])
+                f.close()
+                f = open(file_name+".mother","w+")
+                f.write(vcf[0])
+                f.close()
+                f = open(file_name+".father","w+")
+                f.write(vcf[0])
+                f.close()
+            elif self.inheritance == "BI-PARENTAL":
+                if vcf[2] == "":
+                    f = open(file_name+".child","w+")
+                    f.write(vcf[0]+vcf[1])
+                    f.close()
+                    f = open(file_name+".mother","w+")
+                    f.write(vcf[0]+vcf[1])
+                    f.close()
+                    f = open(file_name+".father","w+")
+                    f.write(vcf[0]+vcf[1])
+                    f.close()
+                else:
+                    f = open(file_name+".child","w+")
+                    f.write(vcf[0]+vcf[1]+vcf[2])
+                    f.close()
+                    f = open(file_name+".mother","w+")
+                    f.write(vcf[0]+vcf[1])
+                    f.close()
+                    f = open(file_name+".father","w+")
+                    f.write(vcf[0]+vcf[2])
+                    f.close()
+            elif self.inheritance == "MATERNAL":
+                f = open(file_name+".child","w+")
+                f.write(vcf[0]+vcf[1]+vcf[2])
+                f.close()
+                f = open(file_name+".mother","w+")
+                f.write(vcf[0]+vcf[1])
+                f.close()
+                f = open(file_name+".father","w+")
+                f.write(vcf[0])
+                f.close()
+            elif self.inheritance == "PATERNAL":
+                f = open(file_name+".child","w+")
+                f.write(vcf[0]+vcf[1]+vcf[2])
+                f.close()
+                f = open(file_name+".mother","w+")
+                f.write(vcf[0])
+                f.close()
+                f = open(file_name+".father","w+")
+                f.write(vcf[0]+vcf[1])
+                f.close()
