9d76123f298daf647c6cfbcee4e512e31dfce89b
Tamar Av-Shalom <avshalom.tamar0@gmail.com>
2018-12-05 10:18:55 -0800

    pulled new version of GUD

--- simulator/transcript.py
+++ simulator/transcript.py
@@ -31,7 +31,7 @@ class Transcript:
             self.txStart    = long(gene.txStart)
             self.txEnd      = long(gene.txEnd)
             self.strand     = gene.strand
-            self.bin        = gene.bin
+            self.bin        = gene.bin ##remove
         except:
             print("Unexpected error, check validity of config and gene inputs")
 
