72e378661680f687afb53ba5a351f2290963f951
Tamar Av-Shalom <avshalom.tamar0@gmail.com>
2019-08-09 11:45:37 -0700

    cnv testing and refactor

--- MenDelSIM/src/copy_number_variant.py
+++ MenDelSIM/src/copy_number_variant.py
@@ -10,26 +10,26 @@ class CopyNumberVariant(Variant):
         self.chrom = self.impact["CHROM"]
         self.start = self.impact["START"]
         self.end = self.impact["END"]
-        self.length = self.impact["COPY_CHANGE"]
+        self.copy_change = self.impact["COPY_CHANGE"]
         self.check_cnv()
-
-
-
-    # TODO: check location
-    def check_location(self):
-        """checks location and converts start to 0 based"""
-        self.start = self.start - 1 
-        self.transcript.get_regionmap()
-
 
     def check_cnv(self):
         """checks all cnv features and fixes to 0 based"""
         if self.type != "CNV":
             raise ValueError("Must be CNV type")
-        self.check_location()
-        if self.length == 0 or self.length < -1 or self.length == 1:
-            raise Exception("Copy change cannot be less than -1 or equal to 1 or 0.")
-
+        if self.copy_change == 0 or self.copy_change < -1 or self.copy_change == 1:
+            raise ValueError("Copy change cannot be less than -1 or equal to 1 or 0.")
+        if type(self.start) is not int or type(self.end) is not int:
+            raise ValueError("start and end must be int.")
+        self.start = self.start-1
+        if self.copy_change < 0: 
+            self.check_location(self.start-1, self.end)
+        else:
+            end = self.end + (self.end - self.start)*self.copy_change
+            self.check_location(self.start, end) 
+        if (self.end - self.start) < 50:
+            raise ValueError("cnvs must be at least 50 bp long") 
+            
     def get_anchor_position(self) -> str:
         """Retrieve a DNA sequence from UCSC.
         Note: UCSC assumes 1 based indexing so we add a 1"""
@@ -38,37 +38,37 @@ class CopyNumberVariant(Variant):
         return seq.upper()
 
     def get_region_seq(self) -> str:
-        """Retrieve a DNA sequence from UCSC.
-        Note: UCSC assumes 1 based indexing so we add a 1"""
         # Get sequence
         seq = self.get_seq(self.chrom, self.start, self.end, self.transcript.get_genome())
         return seq.upper()
 
-    def get_vcf_row(self, transcript: Transcript, format: str = "simple") -> str:
+    def get_vcf_row(self, format: str = "simple") -> str:
         # get regions
         chrom = self.chrom
         pos = str(self.start + 1)  # add 1 to make 1 based
         end = str(self.end)
         distance = int(end) - int(pos)
         if format == "simple":
-            if self.length > 0:
+            if self.copy_change > 0:
                 ref = self.get_region_seq()
-                alt = self.get_region_seq()*self.length
+                alt = self.get_region_seq()*self.copy_change
             else:  # deletion
+                pos = str(self.start)
                 ref = self.get_anchor_position() + self.get_region_seq()
                 alt = self.get_anchor_position()
             info = "."
-        elif format == "4.2":
-            ref = "N"
-            if self.length > 0:  # duplication
-                alt = "<DUP>"
-                end = str(int(pos) + distance*self.length)
-                info = "SVTYPE=DUP;END=" + end + \
-                    ";SVLEN=-"+str(distance*self.length)
-            else:  # deletion   
-                alt = "<DEL>"
-                info = "SVTYPE=DEL;END=" + end + ";SVLEN=-"+str(distance)
-        ID = "_".join(["cnv", pos, str(self.length)])
+        # TODO: fix 4.2 format
+        # elif format == "4.2":
+        #     ref = "N"
+        #     if self.length > 0:  # duplication
+        #         alt = "<DUP>"
+        #         end = str(int(pos) + distance*self.copy_change)
+        #         info = "SVTYPE=DUP;END=" + end + \
+        #             ";SVLEN=-"+str(distance*self.copy_change)
+        #     else:  # deletion   
+        #         alt = "<DEL>"
+        #         info = "SVTYPE=DEL;END=" + end + ";SVLEN=-"+str(distance)
+        ID = "_".join(["cnv", pos, str(self.copy_change)])
         if self.zygosity == "HOMOZYGOUS":
             zygosity = "1/1"
         if self.zygosity == "HEMIZYGOUS":
--- MenDelSIM/tests/test_cnv.py
+++ MenDelSIM/tests/test_cnv.py
@@ -3,27 +3,116 @@ from MenDelSIM.src.copy_number_variant import CopyNumberVariant
 from MenDelSIM.src.transcript import Transcript
 from MenDelSIM.src.api_helper import *
 
+
 class IndelCreationTests(unittest.TestCase):
     XKR8_uid = get_all_transcripts("XKR8", "hg38")[0]["qualifiers"]["uid"]
     transcript = Transcript(XKR8_uid, "hg38")
+
     # test 1
     def test_copy_number_error(self):
-        print(self.transcript.get_regionmap())
-        # cnv = {
-        #     "TYPE": "CNV",
-        #     "REGION": "GENIC",
-        #     "IMPACT": {
-        #         "CHROM": "chr1",
-        #         "START": 27959462,
-        #         "END": 27959762,
-        #         "COPY_CHANGE": 1,  
-        #     },
-        #     "ZYGOSITY": "HETEROZYGOUS"}
-        # with self.assertRaises(ValueError):
-        #     CopyNumberVariant(cnv, self.transcript)
-        # cnv["IMPACT"]["COPY_CHANGE"] = 0
-        # with self.assertRaises(ValueError):
-        #     CopyNumberVariant(cnv, self.transcript)
-        # cnv["IMPACT"]["COPY_CHANGE"] = -3
-        # with self.assertRaises(ValueError):
-        #     CopyNumberVariant(cnv, self.transcript)
+        cnv = {
+            "TYPE": "CNV",
+            "REGION": "GENIC",
+            "IMPACT": {
+                "CHROM": "chr1",
+                "START": 27959462,
+                "END": 27959865,
+                "COPY_CHANGE": 1,
+            },
+            "ZYGOSITY": "HETEROZYGOUS"}
+        with self.assertRaises(ValueError):
+            CopyNumberVariant(cnv, self.transcript)
+        cnv["IMPACT"]["COPY_CHANGE"] = 0
+        with self.assertRaises(ValueError):
+            CopyNumberVariant(cnv, self.transcript)
+        cnv["IMPACT"]["COPY_CHANGE"] = -3
+        with self.assertRaises(ValueError):
+            CopyNumberVariant(cnv, self.transcript)
+
+    # test 2
+    def test_coding_overlap(self):
+        cnv = {
+            "TYPE": "CNV",
+            "REGION": "CODING",
+            "IMPACT": {
+                "CHROM": "chr1",
+                "START": 27960050,
+                "END": 27960150,
+                "COPY_CHANGE": 2,
+            },
+            "ZYGOSITY": "HETEROZYGOUS"}
+        CopyNumberVariant(cnv, self.transcript)
+
+        # test 4
+    def test_UTR_overlap(self):
+        cnv = {
+            "TYPE": "CNV",
+            "REGION": "UTR",
+            "IMPACT": {
+                "CHROM": "chr1",
+                "START": 27959622,
+                "END": 27960150,
+                "COPY_CHANGE": 2,
+            },
+            "ZYGOSITY": "HETEROZYGOUS"}
+        with self.assertRaises(ValueError):
+            CopyNumberVariant(cnv, self.transcript)
+        cnv["IMPACT"]["END"] = 27959682
+        CopyNumberVariant(cnv, self.transcript)
+
+        # test 5
+    def test_intron_overlap(self):
+        cnv = {
+            "TYPE": "CNV",
+            "REGION": "INTRONIC",
+            "IMPACT": {
+                "CHROM": "chr1",
+                "START": 27959999,
+                "END": 27961050,
+                "COPY_CHANGE": 2,
+            },
+            "ZYGOSITY": "HETEROZYGOUS"}
+        with self.assertRaises(ValueError):
+            CopyNumberVariant(cnv, self.transcript)
+        cnv["IMPACT"]["START"] = 27961000
+        CopyNumberVariant(cnv, self.transcript)
+
+        # test 6
+    def test_deletion(self):
+        cnv = {
+            "TYPE": "CNV",
+            "REGION": "INTRONIC",
+            "IMPACT": {
+                "CHROM": "chr1",
+                "START": 27961000,
+                "END": 27961050,
+                "COPY_CHANGE": -1,
+            },
+            "ZYGOSITY": "HETEROZYGOUS"}
+        cnv = CopyNumberVariant(cnv, self.transcript)
+        row = cnv.get_vcf_row().split("\t")
+        self.assertEqual(row[0],"chr1")
+        self.assertEqual(row[1],"27960999")
+        self.assertEqual(row[3],"GCAACCTCAGCTGGCTTCTTGACCTGGGCCTCCCTGGGAGTTTCAGCAGCCC")
+        self.assertEqual(row[4],"G")
+        
+        
+
+        # test 7
+    def test_insertion(self):
+        cnv = {
+            "TYPE": "CNV",
+            "REGION": "INTRONIC",
+            "IMPACT": {
+                "CHROM": "chr1",
+                "START": 27961000,
+                "END": 27961050,
+                "COPY_CHANGE": 2,
+            },
+            "ZYGOSITY": "HETEROZYGOUS"}
+        cnv = CopyNumberVariant(cnv, self.transcript)
+        row = cnv.get_vcf_row().split("\t")
+        self.assertEqual(row[0],"chr1")
+        self.assertEqual(row[1],"27961000")
+        self.assertEqual(row[3],"CAACCTCAGCTGGCTTCTTGACCTGGGCCTCCCTGGGAGTTTCAGCAGCCC")
+        self.assertEqual(row[4],"CAACCTCAGCTGGCTTCTTGACCTGGGCCTCCCTGGGAGTTTCAGCAGCCCCAACCTCAGCTGGCTTCTTGACCTGGGCCTCCCTGGGAGTTTCAGCAGCCC")
