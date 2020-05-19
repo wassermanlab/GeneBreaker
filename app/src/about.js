4be0f70c1ab330697ec339a84d28756ea7dbcc5e
tamario <avshalom.tamar0@gmail.com>
2020-05-19 13:50:42 -0700

    about page template

---
+++ app/src/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './home.css'
+import Nav from './nav';
+
+function About(props) {
+
+  return (
+    <React.Fragment>
+      <Nav/>
+      <div width="100%">
+        <h1>About</h1>
+        <h2>About the tool</h2>
+        <h2>About the authors</h2>
+        <h2>Links</h2>
+        <p></p>
+        
+      </div>
+      </React.Fragment>)
+}
+
+export default About;
